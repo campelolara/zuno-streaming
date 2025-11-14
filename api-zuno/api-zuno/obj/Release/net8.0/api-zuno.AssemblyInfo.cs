e6611f0043f2b0d15488b888bdb0e48f1b911316
Lara <161225698+campelolara@users.noreply.github.com>
2025-11-13 23:00:22 -0300

    a

--- api-zuno/api-zuno/obj/Release/net8.0/api-zuno.AssemblyInfo.cs
+++ api-zuno/api-zuno/obj/Release/net8.0/api-zuno.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-zuno")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b7d8a024d1f4f2b51cf1ef68d580cbc3736eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-zuno")]
