4215521f2959a554acfcc6e5d7d61d7ffbd7dfa6
Lara <161225698+campelolara@users.noreply.github.com>
2025-11-15 21:18:09 -0300

    tela de login

--- zunoandroid/app/src/main/java/br/unip/zunoapp/FormLogin.java
+++ zunoandroid/app/src/main/java/br/unip/zunoapp/FormLogin.java
@@ -1,6 +1,8 @@
 package br.unip.zunoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,9 +23,18 @@ public class FormLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //adiciono um evento de click no textview
+        //intent = msg utilizada para abrir outra tela
+        //sai do formLogin pro FormCadastro
+        texto_tela_cadastro.setOnClickListener(v -> {
+            Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+            startActivity(intent); //navega para a próxima tela
+        });
     }
 
+    //metodo que liga a variavel ao componente de interface dela
     private void IniciarComponentes(){
-        texto
+        texto_tela_cadastro = findViewById(R.id.texto_tela_cadastro);
     }
 }
