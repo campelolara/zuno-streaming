66d75b33d43e42a28466c8680b995765b7222f03
Lara <161225698+campelolara@users.noreply.github.com>
2025-11-18 13:43:14 -0300

    menus

--- zunoandroid/app/src/main/java/br/unip/zunoapp/TelaInicial.java
+++ zunoandroid/app/src/main/java/br/unip/zunoapp/TelaInicial.java
@@ -1,14 +1,25 @@
 package br.unip.zunoapp;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import br.unip.zunoapp.R;
+
 
 public class TelaInicial extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,46 @@ public class TelaInicial extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Bottom Menu
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new Fragmento1()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+
+                int id = item.getItemId();
+
+                if (id == R.id.inicio){
+                    fragment = new Fragmento1();
+                } else if (id == R.id.adicionar){
+                    fragment = new Fragmento2();
+                } else if (id == R.id.perfil) {
+                    fragment = new Fragmento3();
+                } else {
+                    return false;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+
+                return true;
+            }
+        });
+
     }
+
+    //toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
 }
