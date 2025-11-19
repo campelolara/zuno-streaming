b77053f6b18674c1ad09b4d5732c863c72e12557
ceciibat <anaceciliabatalha@gmail.com>
2025-11-19 18:32:52 -0400

    ignora

---
+++ zunoapi/zunoapi/Models2/Criador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace zunoapi.Models2;
+
+[Table("Criador")]
+public partial class Criador
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(70)]
+    [Unicode(false)]
+    public string Email { get; set; } = null!;
+
+    [InverseProperty("Criador")]
+    public virtual ICollection<Conteudo> Conteudos { get; set; } = new List<Conteudo>();
+
+    [InverseProperty("Criador")]
+    public virtual ICollection<Inscricao> Inscricaos { get; set; } = new List<Inscricao>();
+
+    [InverseProperty("Criador")]
+    public virtual ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
+}
---
+++ zunoapi/zunoapi/Models2/Curtidum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace zunoapi.Models2;
+
+public partial class Curtidum
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("UsuarioID")]
+    public int UsuarioId { get; set; }
+
+    [Column("ConteudoID")]
+    public int ConteudoId { get; set; }
+
+    [ForeignKey("ConteudoId")]
+    [InverseProperty("Curtida")]
+    public virtual Conteudo Conteudo { get; set; } = null!;
+
+    [ForeignKey("UsuarioId")]
+    [InverseProperty("Curtida")]
+    public virtual Usuario Usuario { get; set; } = null!;
+}
