9f1657940a33153265b7c6907ee7033a56ab2e87
Lara <161225698+campelolara@users.noreply.github.com>
2025-11-19 13:30:18 -0300

    login page painel do criador

--- painel-de-controle/painel-de-controle/LoginPage.xaml.cs
+++ painel-de-controle/painel-de-controle/LoginPage.xaml.cs
@@ -6,4 +6,25 @@ public partial class LoginPage : ContentPage
 	{
         InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Remove o menu lateral
+        Shell.SetFlyoutBehavior(this, FlyoutBehavior.Disabled);
+
+        // Remove o título da página
+        Shell.SetNavBarIsVisible(this, false);
+    }
+
+    private void OnRegisterLabelTapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void OnLoginButtonCliked(object sender, EventArgs e)
+    {
+
+    }
 }
