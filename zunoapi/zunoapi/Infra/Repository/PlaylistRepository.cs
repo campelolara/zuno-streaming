7e22d193b47b48d0185d24eea44b5e5f3e073cc9
ceciibat <anaceciliabatalha@gmail.com>
2025-11-23 16:14:11 -0400

    correção

--- zunoapi/zunoapi/Infra/Repository/PlaylistRepository.cs
+++ zunoapi/zunoapi/Infra/Repository/PlaylistRepository.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using zunoapi.Infra.Context;
-using zunoapi.Infra.DTO;
 using zunoapi.Infra.Interface;
 using zunoapi.Models;
 
@@ -9,15 +7,8 @@ namespace zunoapi.Infra.Repository
 {
     public class PlaylistRepository : IPlaylistRepository
     {
-<<<<<<< HEAD
-        public readonly ZunoContext _context; 
-        public readonly DbSet<Playlist> _dbSet;       
-=======
-        //referência ao contexto do banco de dados
         protected readonly ZunoContext _context;
-        //referência ao conjunto de entidades do tipo T que serão manipuladas
-        protected readonly DbSet<Playlist> _dbSet;      
->>>>>>> 28598cce56a9a6c2a8ef3176beaddae5901c8929
+        protected readonly DbSet<Playlist> _dbSet;
 
         public PlaylistRepository(ZunoContext context)
         {
@@ -33,7 +24,7 @@ namespace zunoapi.Infra.Repository
         public Playlist GetPlaylistByID(int id)
         {
             return _dbSet.Find(id);
-        
+
         }
 
         public void AddPlaylist(Playlist playlist)
@@ -54,12 +45,7 @@ namespace zunoapi.Infra.Repository
         public async Task Save()
         {
             await _context.SaveChangesAsync();
-<<<<<<< HEAD
         }
-   
 
-=======
-        }    
->>>>>>> 28598cce56a9a6c2a8ef3176beaddae5901c8929
     }
 }
